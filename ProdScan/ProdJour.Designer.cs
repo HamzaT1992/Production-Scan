1106fe5b84523120e0f7a8be555be1d5032ac5cc
HamzaT1992 <tsi.hamza1992@gmail.com>
2019-10-08 13:57:12 +0100

    added waitingbar

--- ProdScan/ProdJour.Designer.cs
+++ ProdScan/ProdJour.Designer.cs
@@ -31,39 +31,77 @@
             Telerik.WinControls.UI.TableViewDefinition tableViewDefinition1 = new Telerik.WinControls.UI.TableViewDefinition();
             this.fluentTheme1 = new Telerik.WinControls.Themes.FluentTheme();
             this.radGridView1 = new Telerik.WinControls.UI.RadGridView();
-            this.dateTimePicker1 = new Telerik.WinControls.UI.RadDateTimePicker();
+            this.radDateTimePicker1 = new Telerik.WinControls.UI.RadDateTimePicker();
+            this.radWaitingBar1 = new Telerik.WinControls.UI.RadWaitingBar();
+            this.fadingRingWaitingBarIndicatorElement1 = new Telerik.WinControls.UI.FadingRingWaitingBarIndicatorElement();
             ((System.ComponentModel.ISupportInitialize)(this.radGridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.radGridView1.MasterTemplate)).BeginInit();
             this.radGridView1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dateTimePicker1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radDateTimePicker1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radWaitingBar1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this)).BeginInit();
             this.SuspendLayout();
             // 
             // radGridView1
             // 
-            this.radGridView1.Controls.Add(this.dateTimePicker1);
+            this.radGridView1.Controls.Add(this.radWaitingBar1);
+            this.radGridView1.Controls.Add(this.radDateTimePicker1);
             this.radGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.radGridView1.Location = new System.Drawing.Point(0, 0);
             // 
             // 
             // 
+            this.radGridView1.MasterTemplate.AllowAddNewRow = false;
+            this.radGridView1.MasterTemplate.AllowDeleteRow = false;
+            this.radGridView1.MasterTemplate.AllowEditRow = false;
+            this.radGridView1.MasterTemplate.AutoSizeColumnsMode = Telerik.WinControls.UI.GridViewAutoSizeColumnsMode.Fill;
+            this.radGridView1.MasterTemplate.MultiSelect = true;
             this.radGridView1.MasterTemplate.ViewDefinition = tableViewDefinition1;
             this.radGridView1.Name = "radGridView1";
+            this.radGridView1.ShowNoDataText = false;
             this.radGridView1.Size = new System.Drawing.Size(568, 323);
             this.radGridView1.TabIndex = 0;
             this.radGridView1.ThemeName = "Fluent";
             // 
             // radDateTimePicker1
             // 
-            this.dateTimePicker1.CalendarSize = new System.Drawing.Size(290, 320);
-            this.dateTimePicker1.Location = new System.Drawing.Point(202, 0);
-            this.dateTimePicker1.Name = "radDateTimePicker1";
-            this.dateTimePicker1.Size = new System.Drawing.Size(164, 24);
-            this.dateTimePicker1.TabIndex = 1;
-            this.dateTimePicker1.TabStop = false;
-            this.dateTimePicker1.Text = "mardi 8 octobre 2019";
-            this.dateTimePicker1.ThemeName = "Fluent";
-            this.dateTimePicker1.Value = new System.DateTime(2019, 10, 8, 12, 21, 0, 822);
+            this.radDateTimePicker1.CalendarSize = new System.Drawing.Size(290, 320);
+            this.radDateTimePicker1.Location = new System.Drawing.Point(202, 0);
+            this.radDateTimePicker1.Name = "radDateTimePicker1";
+            this.radDateTimePicker1.Size = new System.Drawing.Size(164, 24);
+            this.radDateTimePicker1.TabIndex = 1;
+            this.radDateTimePicker1.TabStop = false;
+            this.radDateTimePicker1.Text = "mardi 8 octobre 2019";
+            this.radDateTimePicker1.ThemeName = "Fluent";
+            this.radDateTimePicker1.Value = new System.DateTime(2019, 10, 8, 12, 21, 0, 822);
+            this.radDateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // radWaitingBar1
+            // 
+            this.radWaitingBar1.BackColor = System.Drawing.Color.White;
+            this.radWaitingBar1.Cursor = System.Windows.Forms.Cursors.AppStarting;
+            this.radWaitingBar1.Location = new System.Drawing.Point(249, 126);
+            this.radWaitingBar1.Name = "radWaitingBar1";
+            this.radWaitingBar1.ShowText = true;
+            this.radWaitingBar1.Size = new System.Drawing.Size(70, 70);
+            this.radWaitingBar1.TabIndex = 2;
+            this.radWaitingBar1.Text = "Chargement";
+            this.radWaitingBar1.ThemeName = "Fluent";
+            this.radWaitingBar1.Visible = false;
+            this.radWaitingBar1.WaitingIndicators.Add(this.fadingRingWaitingBarIndicatorElement1);
+            this.radWaitingBar1.WaitingIndicatorSize = new System.Drawing.Size(100, 14);
+            this.radWaitingBar1.WaitingStep = 8;
+            this.radWaitingBar1.WaitingStyle = Telerik.WinControls.Enumerations.WaitingBarStyles.FadingRing;
+            // 
+            // fadingRingWaitingBarIndicatorElement1
+            // 
+            this.fadingRingWaitingBarIndicatorElement1.AccessibleRole = System.Windows.Forms.AccessibleRole.Text;
+            this.fadingRingWaitingBarIndicatorElement1.BackColor2 = System.Drawing.Color.White;
+            this.fadingRingWaitingBarIndicatorElement1.BackColor3 = System.Drawing.Color.White;
+            this.fadingRingWaitingBarIndicatorElement1.BackColor4 = System.Drawing.Color.White;
+            this.fadingRingWaitingBarIndicatorElement1.Name = "fadingRingWaitingBarIndicatorElement1";
+            this.fadingRingWaitingBarIndicatorElement1.Text = "Chargement";
+            this.fadingRingWaitingBarIndicatorElement1.TextImageRelation = System.Windows.Forms.TextImageRelation.TextAboveImage;
             // 
             // ProdJour
             // 
@@ -78,11 +116,13 @@
             this.RootElement.ApplyShapeToControl = true;
             this.Text = "ProdJour";
             this.ThemeName = "Fluent";
+            this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.radGridView1.MasterTemplate)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.radGridView1)).EndInit();
             this.radGridView1.ResumeLayout(false);
             this.radGridView1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dateTimePicker1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radDateTimePicker1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radWaitingBar1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this)).EndInit();
             this.ResumeLayout(false);
 
@@ -92,6 +132,8 @@
 
         private Telerik.WinControls.Themes.FluentTheme fluentTheme1;
         private Telerik.WinControls.UI.RadGridView radGridView1;
-        private Telerik.WinControls.UI.RadDateTimePicker dateTimePicker1;
+        private Telerik.WinControls.UI.RadDateTimePicker radDateTimePicker1;
+        private Telerik.WinControls.UI.RadWaitingBar radWaitingBar1;
+        private Telerik.WinControls.UI.FadingRingWaitingBarIndicatorElement fadingRingWaitingBarIndicatorElement1;
     }
 }
